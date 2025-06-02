6ae2f4a8fb01e26404c88a50f2636db50d7eb4fc
Gizzyaka <gizzatullint@bk.ru>
2025-06-02 17:20:58 +0500

    Оптимизирование кода

---
+++ App/Views/templates/header.php
@@ -0,0 +1,16 @@
+<header class="header">
+    <div class="container">
+        <div class="logo">SneakMax</div>
+        <nav class="navbar">
+            <ul class="navbar-menu">
+                <li class="menu__item"><a href="#" class="menu__link">Каталог</a></li>
+                <li class="menu__item"><a href="#" class="menu__link">О нас</a></li>
+                <li class="menu__item"><a href="#" class="menu__link">Подбор товара</a></li>
+                <li class="menu__item"><a href="#" class="menu__link">Наша команда</a></li>
+                <li class="menu__item"><a href="#" class="menu__link">Доставка и оплата</a></li>
+                <li class="menu__item"><a href="#" class="menu__link">Контакты</a></li>
+                <li class="menu__item"><a href="#" class="menu__link">Корзина</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -2,14 +2,18 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SneakMax Store</title>
-</head>
-<body>
-    <h1>hi this is index page</h1>
+<?php include "App/Views/templates/head.php" ?>
+<?php include "App/Views/templates/header.php" ?>
+<div class="main">
+    <div class="container">
+        <section class="hello">
+            <div class="hello__wrapper">
+                <h1 class="hello__title">Кроссовки известных брендов с доставкой по России и СНГ</h1>
+                <p class="hello__subtitle">Мы продаем кроссовки брендов Nike, Adidas, Puma, Reebok, Converse и многие другие по низким ценам</p>
+                <a href="#" class="btn hello__btn">Перейти к покупкам</a>
+            </div>
+        </section>
+    </div>
+</div>
 </body>
 </html>
