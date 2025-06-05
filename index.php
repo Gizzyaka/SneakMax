61fae035274312bde11ff5b50871b61497450585
Gizzyaka <gizzatullint@bk.ru>
2025-06-05 16:21:54 +0500

    Add about us section

--- index.php
+++ index.php
@@ -153,8 +153,15 @@
         </div>
     </section>
     <section class="about">
-        <div class="container">
+        <div class="about__container container">
+            <h2 class="about__title">Пара слов о нас</h2>
 
+            <p class="about__text">Спорт держит нас в форме. Учит дисциплине. Объединяет нас. Через спорт мы можем менять жизни. В том числе с помощью воодушевляющих историй спортсменов. Чтобы помочь тебе подняться и двигаться вперед.</p>
+
+            <div class="about__signature">
+                <div class="about__dash"></div>
+                <span>SneakMax</span>
+            </div>
         </div>
     </section>
 </div>
